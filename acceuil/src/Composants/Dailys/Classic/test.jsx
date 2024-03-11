d52391cf07a4a5ddf72eab26bc5e28b951874baf
Elias Djebbour <djebbour2912@gmail.com>
2024-03-11 22:21:18 +0100

    mise a jour de test.jsx

--- acceuil/src/Composants/Dailys/Classic/test.jsx
+++ acceuil/src/Composants/Dailys/Classic/test.jsx
@@ -8,6 +8,20 @@ function Test() {
   const [pokemonName, setPokemonName] = useState('');
   const [pokemonDataList, setPokemonDataList] = useState([]);
 
+  const getRandomPokemon = (pokemonNames) => {
+    const pokemonKeys = Object.keys(pokemonNames);
+    const randomKey = pokemonKeys[Math.floor(Math.random() * pokemonKeys.length)];
+    const randomPokemon = {
+      frenchName: randomKey,
+      englishName: pokemonNames[randomKey],
+    };
+    console.log(randomPokemon.frenchName)
+    return randomPokemon;
+  };
+
+  const dailyPokemon = getRandomPokemon(pokemonNames);
+  console.log(dailyPokemon)
+
   const handleInputChange = (event) => {
     setPokemonName(event.target.value);
   };
@@ -46,6 +60,39 @@ function Test() {
     }
   };
 
+  const getEvolutionChain = async (species) => {
+    try {
+      const evolutionChainUrl = species.data.evolution_chain.url;
+      
+      const evolutionChainResponse = await axios.get(evolutionChainUrl);
+      let evolv_stage = 1;
+      
+      // Fonction récursive pour calculer le stade d'évolution
+      const calculateEvolutionStage = (chain) => {
+        console.log(chain.evolves_to && chain.evolves_to.length > 0);
+        if (chain.species.name !== species.data.name) {
+          evolv_stage += 1;
+        } else {
+          return evolv_stage;
+        }
+  
+        if (chain.evolves_to && chain.evolves_to.length > 0) {
+          chain.evolves_to.forEach((evolution) => {
+            calculateEvolutionStage(evolution);
+          });
+        }
+      };
+  
+      // Appel initial de la fonction pour démarrer le processus
+      calculateEvolutionStage(evolutionChainResponse.data.chain);
+  
+      return evolv_stage;
+    } catch (error) {
+      console.error('Erreur lors de la récupération de la chaîne d\'évolution :', error.message);
+      return [];
+    }
+  };
+
   const getGenerationByPokemonId = (pokemonId) => {
     if (pokemonId <= 151) {
       return 1;
@@ -79,28 +126,40 @@ function Test() {
       try {
         const englishName = getEnglishPokemonName(pokemonName);
         const pokemonDetailsResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon/${englishName.toLowerCase()}`);
+
+        const sprite = pokemonDetailsResponse.data.sprites.front_default;
+
         const speciesResponse = await axios.get(pokemonDetailsResponse.data.species.url);
 
         const types = await Promise.all(pokemonDetailsResponse.data.types.map(async (type) => await getTypeNameInFrench(type.type.name)));
         const habitatInFrench = await getHabitatNameInFrench(speciesResponse.data.habitat ? speciesResponse.data.habitat.name : 'Inconnu');
         const colorsInFrench = await getColorNameInFrench(speciesResponse.data.color.name);
+        console.log(speciesResponse.data.name)
 
         const height = pokemonDetailsResponse.data.height;
         const weight = pokemonDetailsResponse.data.weight;
         const pokemonId = pokemonDetailsResponse.data.id;
         const generation = getGenerationByPokemonId(pokemonId);
+        const evolutionChain = await getEvolutionChain(speciesResponse);
 
         const newPokemonData = {
           name: pokemonName,
+          sprite: sprite,
           types: types,
           habitat: habitatInFrench,
           colors: colorsInFrench,
           height: height,
           weight: weight,
+          evolutionChain : evolutionChain,
           generation: generation,
         };
 
-        setPokemonDataList((prevList) => [...prevList, newPokemonData]);
+        // Créez une nouvelle copie du tableau avec le nouvel élément au début
+        const updatedList = [newPokemonData, ...pokemonDataList];
+
+        // Mettez à jour l'état avec la nouvelle copie du tableau
+        setPokemonDataList(updatedList);
+
         setPokemonName('');
       } catch (error) {
         console.error('Erreur lors de la requête à Pokeapi :', error.message);
@@ -113,13 +172,16 @@ function Test() {
       <div className='div-container'>
         {pokemonDataList.map((pokemonData, index) => (
           <div key={index} className="dynamic-div">
-            <p>Nom du Pokémon: {pokemonData.name}</p>
-            <p>Types: {pokemonData.types.join(', ')}</p>
-            <p>Habitat: {pokemonData.habitat}</p>
-            <p>Couleurs: {pokemonData.colors}</p>
-            <p>Hauteur: {pokemonData.height * 10} cm</p>
-            <p>Poids: {pokemonData.weight / 10} kg</p>
-            <p>Generation: {pokemonData.generation}</p>
+            <p><img src={pokemonData.sprite} alt={pokemonData.name} 
+                style={{ width: '300px', height: '300px' }} /></p>
+            <p>{pokemonData.types[0]}</p>
+            <p>{pokemonData.types[1] || 'Aucun'}</p>
+            <p>{pokemonData.habitat}</p>
+            <p>{pokemonData.colors}</p>
+            <p>{pokemonData.evolutionChain}</p>
+            <p>{pokemonData.height * 10} cm</p>
+            <p>{pokemonData.weight / 10} kg</p>
+            <p>{pokemonData.generation}</p>
           </div>
         ))}
       </div>
